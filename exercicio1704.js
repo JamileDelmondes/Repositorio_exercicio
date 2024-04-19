7a20d42550a43792d868ee48366c8d1590d9f01c
JamileDelmondes <159553162+JamileDelmondes@users.noreply.github.com>
2024-04-19 18:46:38 -0300

    update
    19/04/2024

--- exercicio1704.js
+++ exercicio1704.js
@@ -1,14 +1,36 @@
-const nome = "Firmina";
-const sexo = "F";
-const idade = 87;
-const contribuicao = 60;
+const pessoa = [
+    {
+    nome:"Emília",
+    sexo: "F",
+    idade: 59,
+    contribuicao:40,
+    }
+]
 
-for(sexo="M";){
-    if(idade+contribuicao<85){
-        console.log("${nome}, você pode se aposentar!")
+if(pessoa[sexo]==="M"){
+    if(pessoa[contribuicao]>=35){
+        if(pessoa[idade]+pessoa[contribuicao]>95){
+        console.log(`${pessoa[nome]}, você pode se aposentar!`)
+        }
+        else{
+        console.log(`${pessoa[nome]}, você ainda não completou o tempo de contribuição!`)
+        }
     }
     else{
-        console.log("${nome} você ainda não pode aposentar!")
+    console.log(`${pessoa[nome]}, você ainda não completou o tempo de contribuição!`)
+    }
+}
+else{
+    if(pessoa[contribuicao]>=30){
+        if(pessoa[idade]+pessoa[contribuicao]>85){
+        console.log(`${pessoa[nome]}, você pode se aposentar!`)
+        }
+        else{
+        console.log(`${pessoa[nome]}, você ainda não completou o tempo de contribuição!`)
+        }
+    }
+    else{
+    console.log(`${pessoa[nome]}, você ainda não completou o tempo de contribuição!`)
     }
 }
 
