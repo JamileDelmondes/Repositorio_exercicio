84b923563caa0eb657c8e4e523ae5bc17bc4fecb
JamileDelmondes <159553162+JamileDelmondes@users.noreply.github.com>
2024-04-19 19:05:36 -0300

    update
    19/04/2024

--- exercicio1704.js
+++ exercicio1704.js
@@ -1,36 +1,34 @@
-const pessoa = [
-    {
-    nome:"Emília",
-    sexo: "F",
-    idade: 59,
-    contribuicao:40,
-    }
-]
+const nome = "Emília"
+const sexo =  "F"
+const idade = 40
+const contribuicao = 22
+    
 
-if(pessoa[sexo]==="M"){
-    if(pessoa[contribuicao]>=35){
-        if(pessoa[idade]+pessoa[contribuicao]>95){
-        console.log(`${pessoa[nome]}, você pode se aposentar!`)
+
+if(sexo==="M"){
+    if(contribuicao>=35){
+        if(idade+contribuicao>95){
+        console.log(`${nome}, você pode se aposentar!`)
         }
         else{
-        console.log(`${pessoa[nome]}, você ainda não completou o tempo de contribuição!`)
+        console.log(`${nome}, você ainda não completou o tempo de contribuição!`)
         }
     }
     else{
-    console.log(`${pessoa[nome]}, você ainda não completou o tempo de contribuição!`)
+    console.log(`${nome}, você ainda não completou o tempo de contribuição!`)
     }
 }
 else{
-    if(pessoa[contribuicao]>=30){
-        if(pessoa[idade]+pessoa[contribuicao]>85){
-        console.log(`${pessoa[nome]}, você pode se aposentar!`)
+    if(contribuicao>=30){
+        if(idade+contribuicao>85){
+        console.log(`${nome}, você pode se aposentar!`)
         }
         else{
-        console.log(`${pessoa[nome]}, você ainda não completou o tempo de contribuição!`)
+        console.log(`${nome}, você ainda não completou o tempo de contribuição!`)
         }
     }
     else{
-    console.log(`${pessoa[nome]}, você ainda não completou o tempo de contribuição!`)
+    console.log(`${nome}, você ainda não completou o tempo de contribuição!`)
     }
 }
 
