ab6d1bd304897ea4f72caf95de6a7643e11103fa
JamileDelmondes <159553162+JamileDelmondes@users.noreply.github.com>
2024-04-19 15:56:06 -0300

    att exercicio
    17/04/2024

--- exercicio1704.js
+++ exercicio1704.js
@@ -3,4 +3,12 @@ const sexo = "F";
 const idade = 87;
 const contribuicao = 60;
 
+for(sexo="M";){
+    if(idade+contribuicao<85){
+        console.log("${nome}, você pode se aposentar!")
+    }
+    else{
+        console.log("${nome} você ainda não pode aposentar!")
+    }
+}
 
